5ee540aca8fd62d201b3ea3a93067b7b2176985e
renchao <renchao@menusifu.cn>
2026-06-25 13:24:27 +0800

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: process.env.POS_BASE_URL ?? 'http://192.168.0.72:22080',
+    headless: false,
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
